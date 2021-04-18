a163b6fea79a6b8ec2d811cce0c9b94518cafcfb
XueMei-L <72436762+XueMei-L@users.noreply.github.com>
2021-04-19 00:56:08 +0200

    Delete src directory

--- src/Foods/fruits.ts
+++
@@ -1,9 +0,0 @@
-import { Food } from '../food';
-import { macroT, locationT } from '../helpers';
-
-export class Fuits extends Food {
-    constructor(name: string, origin:locationT, macros:macroT,
-        weight: number, Kcal100gr:number, priceKg:number) {
-        super(name, origin, macros, weight, Kcal100gr, priceKg);
-    }
-}
--- src/Foods/legume.ts
+++
@@ -1,9 +0,0 @@
-import { Food } from '../food';
-import { macroT, locationT } from '../helpers';
-
-export class Legume extends Food {
-    constructor(name: string, origin:locationT, macros:macroT,
-        weight: number, Kcal100gr:number, priceKg:number) {
-        super(name, origin, macros, weight, Kcal100gr, priceKg);
-    }
-}
